ea84201bfd2d5444278dce25084f9fc4cdf8c4e5
Dibyanshu <dibyansh01iitk@gmail.com>
2023-10-08 09:36:42 +0530

    mobile-filter, preview modal zustand

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,9 +5,11 @@ import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
-import Filter from "./components/filter";
 import NoResult from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
+
+import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -39,6 +41,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             <div className="px-4 sm:px-6 lg:px-8 pb-24">
                 <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                     {/* mobile filter */}
+                    <MobileFilters sizes={sizes} colors={colors}/>
                     <div className="hidden lg:block">
                         <Filter 
                             valueKey="sizeId"
